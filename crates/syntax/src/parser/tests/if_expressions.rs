3c9111509e48eb2f3da99df99c343e1bb7798ee0
Colton Donnelly <colton@donn.io>
2023-02-23 09:12:05 -0800

    fix if-else chains thanks copilot for refactoring my tests for me

--- crates/syntax/src/parser/tests/if_expressions.rs
+++ crates/syntax/src/parser/tests/if_expressions.rs
@@ -59,8 +59,8 @@ fn if_expression() {
 					b_curly_r(6, 7)
 				]),
 				WHITESPACE(7, 8),
-				if_expression_else(8, 15, [
-					kw_else(8, 12),
+				kw_else(8, 12),
+				else_clause(12, 15, [
 					block(12, 15, [
 						b_curly_l(12, 13),
 						statements(13, 14, [
@@ -104,9 +104,9 @@ fn if_expression() {
 					b_curly_r(6, 7)
 				]),
 				WHITESPACE(7, 8),
-				if_expression_else(8, 20, [
-					kw_else(8, 12),
-					WHITESPACE(12, 13),
+				kw_else(8, 12),
+				WHITESPACE(12, 13),
+				else_clause(13, 20, [
 					if_expression(13, 20, [
 						kw_if(13, 15),
 						WHITESPACE(15, 16),
@@ -134,73 +134,74 @@ fn if_expression() {
 		]
 	};
 
-	// parses_to! {
-	// 	parser: MistParser,
-	// 	input: "if 0{1} elseif 2{3}else{4}",
-	// 	rule: Rule::if_expression,
-	// 	tokens: [
-	// 		if_expression(0, 26, [
-	// 			kw_if(0, 2),
-	// 			WHITESPACE(2, 3),
-	// 			expression(3, 4, [
-	// 				expression_without_block(3, 4, [
-	// 					literal(3, 4, [
-	// 						literal_integer(3, 4)
-	// 					])
-	// 				])
-	// 			]),
-	// 			block(4, 7, [
-	// 				b_curly_l(4, 5),
-	// 				statements(5, 6, [
-	// 					expression_without_block(5, 6, [
-	// 						literal(5, 6, [
-	// 							literal_integer(5, 6)
-	// 						])
-	// 					])
-	// 				]),
-	// 				b_curly_r(6, 7)
-	// 			]),
-	// 			WHITESPACE(7, 8),
-	// 			if_expression_else(8, 26, [
-	// 				kw_else(8, 12),
-	// 				if_expression(12, 26, [
-	// 					kw_if(12, 14),
-	// 					WHITESPACE(14, 15),
-	// 					expression(15, 16, [
-	// 						expression_without_block(15, 16, [
-	// 							literal(15, 16, [
-	// 								literal_integer(15, 16)
-	// 							])
-	// 						])
-	// 					]),
-	// 					block(16, 20, [
-	// 						b_curly_l(16, 17),
-	// 						statements(17, 18, [
-	// 							expression_without_block(17, 18, [
-	// 								literal(17, 18, [
-	// 									literal_integer(17, 18)
-	// 								])
-	// 							])
-	// 						]),
-	// 						b_curly_r(18, 19)
-	// 					]),
-	// 					if_expression_else(19, 26, [
-	// 						kw_else(19, 23),
-	// 						block(23, 26, [
-	// 							b_curly_l(23, 24),
-	// 							statements(24, 25, [
-	// 								expression_without_block(24, 25, [
-	// 									literal(24, 25, [
-	// 										literal_integer(24, 25)
-	// 									])
-	// 								])
-	// 							]),
-	// 							b_curly_r(25, 26)
-	// 						])
-	// 					])
-	// 				])
-	// 			])
-	// 		])
-	// 	]
-	// };
+	parses_to! {
+		parser: MistParser,
+		input: "if 0{1} else if 2{3}else{4}",
+		rule: Rule::if_expression,
+		tokens: [
+			if_expression(0, 27, [
+				kw_if(0, 2),
+				WHITESPACE(2, 3),
+				expression(3, 4, [
+					expression_without_block(3, 4, [
+						literal(3, 4, [
+							literal_integer(3, 4)
+						])
+					])
+				]),
+				block(4, 7, [
+					b_curly_l(4, 5),
+					statements(5, 6, [
+						expression_without_block(5, 6, [
+							literal(5, 6, [
+								literal_integer(5, 6)
+							])
+						])
+					]),
+					b_curly_r(6, 7)
+				]),
+				WHITESPACE(7, 8),
+				kw_else(8, 12),
+				WHITESPACE(12, 13),
+				else_clause(13, 27, [
+					if_expression(13, 27, [
+						kw_if(13, 15),
+						WHITESPACE(15, 16),
+						expression(16, 17, [
+							expression_without_block(16, 17, [
+								literal(16, 17, [
+									literal_integer(16, 17)
+								])
+							])
+						]),
+						block(17, 20, [
+							b_curly_l(17, 18),
+							statements(18, 19, [
+								expression_without_block(18, 19, [
+									literal(18, 19, [
+										literal_integer(18, 19)
+									])
+								])
+							]),
+							b_curly_r(19, 20)
+						]),
+						kw_else(20, 24),
+						else_clause(24, 27, [
+							block(24, 27, [
+								b_curly_l(24, 25),
+								statements(25, 26, [
+									expression_without_block(25, 26, [
+										literal(25, 26, [
+											literal_integer(25, 26)
+										])
+									])
+								]),
+								b_curly_r(26, 27)
+							])
+						])
+					])
+				])
+			])
+		]
+	};
 }
