c125912822c6cc99ae444be0c162fa1358fb60b3
Colton Donnelly <colton@donn.io>
2023-03-28 00:35:52 -0700

    add pre-commit hooks

--- devenv.nix
+++ devenv.nix
@@ -1,8 +1,17 @@
-{ ... }: {
-	languages = {
-		rust = {
-			enable = true;
-			version = "latest";
-		};
-	};
+{...}: {
+  languages = {
+    rust = {
+      enable = true;
+      version = "latest";
+    };
+  };
+
+  pre-commit.hooks = {
+    alejandra.enable = true;
+    cargo-check.enable = true;
+    clippy.enable = true;
+    editorconfig-checker.enable = true;
+    markdownlint.enable = true;
+    yamllint.enable = true;
+  };
 }
